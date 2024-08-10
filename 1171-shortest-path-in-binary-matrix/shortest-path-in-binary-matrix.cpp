b71aebb931769bbf2ce5ded9318c008528958f2c
yatharthg07 <99501437+yatharthg07@users.noreply.github.com>
2024-08-11 02:48:40 +0530

    Time: 205 ms (8.71%) | Memory: 39.7 MB (13.47%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -27,6 +27,11 @@ public:
             int cordy = q.front().first.second;
             int dist=q.front().second;
             q.pop();
+
+            if(dist!=mp[{cordx,cordy}])
+            {
+                continue;
+            }
             for(int i=-1;i<=1;i++)
             {
                 for(int j=-1;j<=1;j++)
