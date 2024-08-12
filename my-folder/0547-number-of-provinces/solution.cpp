0634afdbd3b2729c1890cd2696a3120d7e0c7116
yatharthg07 <99501437+yatharthg07@users.noreply.github.com>
2024-08-12 21:45:26 +0000

    [LeetCode Sync] Runtime - 21 ms (59.61%), Memory - 18.6 MB (62.68%)

--- my-folder/0547-number-of-provinces/solution.cpp
+++ my-folder/0547-number-of-provinces/solution.cpp
@@ -1,29 +1,79 @@
+class DisjointSet {
+    vector<int> rank, parent, size;
+public:
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
+
 class Solution {
 public:
-    void dfs(vector<vector<int>>& M, vector<int>& vis, int i)
-    {
-	    vis[i]=1;
-	    for(int j=0;j<M.size();j++)
-		    if(M[i][j]==1 && !vis[j])
-			dfs(M,vis,j);
-    }   
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int n=isConnected.size();
-        int ans=0;
-	    if(n==0) return 0;
 
-	    vector<int>vis(n,0);
+       
+        int n=isConnected.size();
+        DisjointSet ds(n);
+        int comp=n;
+        vector<pair<int,int>> edges;
         for(int i=0;i<n;i++)
-	    {
-		    if(!vis[i])
-		    {
-			    ans++;
-			    dfs(isConnected,vis,i);
-		    }
-	    }
-	    return ans;
-        
-        
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(isConnected[i][j]==1)
+                {
+                    if(ds.findUPar(i)!=ds.findUPar(j))
+                    {
+                        ds.unionByRank(i,j);
+                        comp--;
+                    }
+                }
+            }
+        }
+return comp;
+
         
     }
 };
