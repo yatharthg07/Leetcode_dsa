b3aa58659f3b7a997c768ec45a3c125d7d7aa93a
yatharthg07 <99501437+yatharthg07@users.noreply.github.com>
2024-08-16 19:14:22 +0530

    Time: 394 ms (28.15%) | Memory: 151.3 MB (39.18%) - LeetSync

--- 854-making-a-large-island/making-a-large-island.cpp
+++ 854-making-a-large-island/making-a-large-island.cpp
@@ -1,97 +1,38 @@
-class DisjointSet {
-    vector<int> rank, parent, size;
-    int max_size = 1;
-public:
-    DisjointSet(int n) {
-        rank.resize(n + 1, 0);
-        parent.resize(n + 1);
-        size.resize(n + 1);
-        for (int i = 0; i <= n; i++) {
-            parent[i] = i;
-            size[i] = 1;
-        }
-    }
-
-    int findUPar(int node) {
-        if (node == parent[node])
-            return node;
-        return parent[node] = findUPar(parent[node]);
-    }
-
-    void unionBySize(int u, int v) {
-        int root_u = findUPar(u);
-        int root_v = findUPar(v);
-        if (root_u == root_v) return;
-        if (size[root_u] < size[root_v]) {
-            parent[root_u] = root_v;
-            size[root_v] += size[root_u];
-            max_size = max(max_size, size[root_v]);
-        }
-        else {
-            parent[root_v] = root_u;
-            size[root_u] += size[root_v];
-            max_size = max(max_size, size[root_u]);
-        }
-    }
-
-    int maxx() {
-        return max_size;
-    }
-
-    int sizee(int p) {
-        return size[p];
-    }
-};
-
-vector<int> d = {-1, 0, 1, 0, -1};
-
 class Solution {
 public:
+    const int DIR[5] = {0, 1, 0, -1, 0};
+    int m, n;
+    unordered_map<int, int> componentSize;
     int largestIsland(vector<vector<int>>& grid) {
-        int n = grid.size();
-        DisjointSet ds(n * n);
-
-        auto is_valid = [&](int x, int y) {
-            return x >= 0 && x < n && y >= 0 && y < n;
-        };
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1) {
-                    int nodeno = n * i + j;
-                    for (int k = 0; k < 4; k++) {
-                        int nx = i + d[k], ny = j + d[k + 1];
-                        if (is_valid(nx, ny) && grid[nx][ny] == 1) {
-                            int adjno = n * nx + ny;
-                            ds.unionBySize(nodeno, adjno);
-                        }
-                    }
-                }
+        m = grid.size(); n = grid[0].size();
+        int ans = 0, nextColor = 2;
+        for (int r = 0; r < m; ++r) {
+            for (int c = 0; c < n; ++c) {
+                if (grid[r][c] != 1) continue; // Only paint when it's an island cell
+                paint(grid, r, c, nextColor++);
+                ans = max(ans, componentSize[nextColor - 1]);
             }
         }
-
-        if (ds.maxx() == n * n) return n * n;
-
-        int ans = ds.maxx();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 0) {
-                    unordered_set<int> par;
-                    int sum = 1;
-                    for (int k = 0; k < 4; k++) {
-                        int nx = i + d[k], ny = j + d[k + 1];
-                        if (is_valid(nx, ny) && grid[nx][ny] == 1) {
-                            int root = ds.findUPar(n * nx + ny);
-                            if (par.insert(root).second) {
-                                sum += ds.sizee(root);
-                            }
-                        }
-                    }
-                    ans = max(ans, sum);
+        for (int r = 0; r < m; ++r) {
+            for (int c = 0; c < n; ++c) {
+                if (grid[r][c] != 0) continue; // Skip non-empty cell
+                unordered_set<int> neighborColors;
+                for (int i = 0; i < 4; ++i) {
+                    int nr = r + DIR[i], nc = c + DIR[i+1];
+                    if (nr < 0 || nr == m || nc < 0 || nc == n || grid[nr][nc] == 0) continue;
+                    neighborColors.insert(grid[nr][nc]);
                 }
+                int sizeFormed = 1;
+                for (int color : neighborColors) sizeFormed += componentSize[color];
+                ans = max(ans, sizeFormed);
             }
         }
-
         return ans;
+    }
+    void paint(vector<vector<int>>& grid, int r, int c, int color) {
+        if (r < 0 || r == m || c < 0 || c == n || grid[r][c] != 1) return;
+        grid[r][c] = color;
+        componentSize[color] += 1;
+        for (int i = 0; i < 4; ++i) paint(grid, r + DIR[i], c + DIR[i+1], color);
     }
 };
