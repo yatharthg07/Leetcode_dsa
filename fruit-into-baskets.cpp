827a9139500899760a3484f93c0107d1c2fa5043
yatharthg07 <99501437+yatharthg07@users.noreply.github.com>
2024-08-18 23:17:51 +0530

    Add fruit-into-baskets.cpp

---
+++ fruit-into-baskets.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int n=fruits.size();
+        int l=0;
+        int r=0;
+        unordered_map<int,int> mp;
+        int count=0;
+        int maxlen=0;
+        while(r<n)
+        {
+            if(mp.find(fruits[r])==mp.end())
+            {
+                mp[fruits[r]]=1;
+                count++;
+            }
+            else
+            {
+                mp[fruits[r]]++;
+            }
+            while(count>2)
+            {
+                if(mp[fruits[l]]==1)
+                {
+                    mp.erase(fruits[l]);
+                    count--;
+                }
+                else
+                    mp[fruits[l]]--;  
+                l++;      
+
+            }
+            maxlen=max(maxlen,r-l+1);
+            r++;
+
+            
+        }
+        return maxlen;
+        
+    }
+};
