38db8a9f48243fd428718db0193881e3a753e4e6
yatharthg07 <99501437+yatharthg07@users.noreply.github.com>
2024-08-19 00:50:05 +0530

    Time: 62 ms (16.52%), Space: 17924000 (6.07%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
@@ -1,32 +1,32 @@
-#define ll long long
 class Solution {
 public:
+    int plzhelp(string s, int k) {
+    
+        int i = 0;
+        int j = 0;
+        unordered_map<char, int> mp;
+        int count = 0;
+
+        while (j < s.length()) {
+            mp[s[j]]++;
+
+            while (mp.size() > k) {
+                mp[s[i]]--;
+                if (mp[s[i]] == 0) {
+                    mp.erase(s[i]);
+                }
+                i++;
+            }
+
+            count += (j - i + 1);
+            j++;
+        }
+        return count;
+    }
+
     int numberOfSubstrings(string s) {
-      ll n=s.size();
-      ll total=n*(n+1)/2;
-      
-      //now write code to find for at most 2 distinct characters
-      ll acnt=0,bcnt=0,ccnt=0,res=0,l=0,r=0;
-      
-      while(r<n){
-          if(s[r]=='a')acnt++;
-          if(s[r]=='b')bcnt++;
-          if(s[r]=='c')ccnt++;
-          
-          while(acnt>0 && bcnt>0 && ccnt>0){
-               if(s[l]=='a')acnt--;
-               if(s[l]=='b')bcnt--;
-               if(s[l]=='c')ccnt--;
-              l++;
-          }
-          
-          res+=(r-l+1);
-          
-        r++;  
-      }
-      
-      
-      return total-res;      //total no of subarrays-subarrys with at most two distinct
-        
+        int k = 3;
+        int count = plzhelp(s, k) - plzhelp(s, k - 1);
+        return count;
     }
 };
