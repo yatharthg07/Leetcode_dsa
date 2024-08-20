e5bbe56af49dc0b536070649de9b2db696081fe9
yatharthg07 <99501437+yatharthg07@users.noreply.github.com>
2024-08-21 03:12:41 +0530

    Time: 89 ms (72.83%), Space: 70084000 (67.62%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
-    int help(vector<int> & nums,int k)
-    {
-        int l=0;
-        int r=0;
-        int n=nums.size();
-        int count=0;
-        int res=0;
-        while(r<n)
-        {
-            count += nums[r]%2==0?0:1;
-            while(count>k)
-            {
-                count-=nums[l]%2==0?0:1;
-                l++;
-            }
-            res+=r-l+1;
-            r++;
-
-        }
-        return res;
-    }
     int numberOfSubarrays(vector<int>& nums, int k) {
-        return help(nums,k)-help(nums,k-1);
-        
+        int subarrays = 0, initialGap = 0, qsize = 0, start = 0;
+        for (int end = 0; end < nums.size(); end++) {
+            // If current element is odd, increment qsize by 1.
+            if (nums[end] % 2 == 1) {
+                qsize++;
+            }
+            // If qsize is k, calculate initialGap and add it in the answer.
+            if (qsize == k) {
+                initialGap = 0;
+                // Calculate the number of even elements in the beginning of
+                // subarray.
+                while (qsize == k) {
+                    qsize -= nums[start] % 2;
+                    initialGap++;
+                    start++;
+                }
+            }
+            subarrays += initialGap;
+        }
+
+        return subarrays;
     }
 };
