5dfbc6c4d5e6c2d19396922a2adc17bcd893e0b8
yatharthg07 <99501437+yatharthg07@users.noreply.github.com>
2024-08-21 05:01:03 +0530

    Time: 8 ms (81.48%) | Memory: 10 MB (78.69%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,49 +1,40 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        pair<int,int> idx;
-        int l=0;int r=0;int n=s.length();
-        unordered_map<char,int> mp;
-        for(auto c:t)
-        {
-            mp[c]++;
+        pair<int, int> idx;
+        int l = 0, r = 0, n = s.length();
+        vector<int> hash(128, 0); 
+        for (char c : t) {
+            hash[c]++;
         }
-        int count=0;
-        int len=t.length();
-        int res=INT_MAX;
-        while(r<n)
-        {   
-            if(mp.find(s[r])!=mp.end()){
-            if(mp[s[r]]>0)
+        int count = 0;
+        int len = t.length();
+        int res = INT_MAX;
+
+        while (r < n) {
+            if (hash[s[r]] > 0) {
                 count++;
-            mp[s[r]]--;
             }
-            while(count==len)
-            {
-                if(r-l+1<res)
-                {
-                    res=r-l+1;
-                    idx.first=l;
-                    idx.second=r;
+            hash[s[r]]--;
+
+            while (count == len) {
+                if (r - l + 1 < res) {
+                    res = r - l + 1;
+                    idx.first = l;
+                    idx.second = r;
                 }
-                if(mp.find(s[l])!=mp.end()){
-                    mp[s[l]]++;
-                    if(mp[s[l]]>0)
-                    {
-                        count--;
-                    }
+                hash[s[l]]++;
+                if (hash[s[l]] > 0) {
+                    count--;
                 }
                 l++;
             }
             r++;
-
-
         }
-        if(res!=INT_MAX)
-        return s.substr(idx.first,idx.second-idx.first+1);
+
+        if (res != INT_MAX)
+            return s.substr(idx.first, idx.second - idx.first + 1);
         else
             return "";
-
-        
     }
 };
