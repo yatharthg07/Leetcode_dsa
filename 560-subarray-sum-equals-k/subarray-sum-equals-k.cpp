4717fa47fda6516aed17412f6c11cc1a5ca48411
yatharthg07 <99501437+yatharthg07@users.noreply.github.com>
2024-08-22 20:34:44 +0530

    Time: 933 ms (24.42%) | Memory: 105.9 MB (6.88%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,39 +1,31 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int n=nums.size();
+        int n = nums.size();
+        vector<int> presum(n);  
+        presum[0] = nums[0];
 
-        int presum;
-        presum=0;
-        unordered_map<int,int> mp;
-        mp[0]++;
-        int cnt=0;
-        for(int i=0;i<n;i++)
-        {
-            presum+=nums[i];
-            cnt+=mp[presum-k];
-            mp[presum]++;
-            
+        unordered_map<int, unordered_set<int>> mp;
+        mp[0].insert(-1);  
+        mp[presum[0]].insert(0);
 
+        for (int i = 1; i < n; i++) {
+            presum[i] = presum[i - 1] + nums[i];
+            mp[presum[i]].insert(i);
         }
-        // for(int i=0;i<n;i++)
-        // {
-        //     if(mp.find(presum[i]-k)!=mp.end())
-        //     {
-        //         auto st=mp[presum[i]-k];
-        //         for(auto it:st)
-        //         {
-        //             if(it<i)
-        //             {
-        //                 cnt++;
-        //             }
-        //         }
-        //     }
-        // }
+
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            int target = presum[i] - k;
+            if (mp.find(target) != mp.end()) {
+                for (auto it : mp[target]) {
+                    if (it < i) {
+                        cnt++;
+                    }
+                }
+            }
+        }
+
         return cnt;
-
-
-
-        
     }
 };
