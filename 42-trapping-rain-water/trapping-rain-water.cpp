5f34e09a5bfcd10ca287368de0640af54251688e
yatharthg07 <99501437+yatharthg07@users.noreply.github.com>
2024-08-28 02:04:40 +0530

    Time: 11 ms (60.09%) | Memory: 25 MB (19.16%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,44 +1,20 @@
 class Solution {
 public:
-    int trap(vector<int>& height) {
-        int n=height.size();
-        vector<int> ans(n,0);
-        vector<int> left(n,-1);
-        vector<int> right(n,-1);
-        int lg=-1;
-        int rg=-1;
-        for(int i=n-1;i>=0;i--)
-        {   
-            if(height[i]>rg)
-            {
-                rg=height[i];
-            }
-            else if(rg>height[i])
-            {
-                right[i]=rg;
-            }
+    int trap(vector<int>& arr) {
+        int n = arr.size();
+        int prefix[n], suffix[n];
+        prefix[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            prefix[i] = max(prefix[i - 1], arr[i]);
         }
-        for(int i=0;i<n;i++)
-        {   
-            if(height[i]>lg)
-            {
-                lg=height[i];
-            }
-            else if(lg>height[i])
-            {
-                left[i]=lg;
-            }
+        suffix[n - 1] = arr[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            suffix[i] = max(suffix[i + 1], arr[i]);
         }
-
-        for(int i=0;i<n;i++)
-        {
-            if(left[i]==-1||right[i]==-1)
-            {
-                continue;
-            }
-            ans[i]=min(left[i],right[i])-height[i];
+        int waterTrapped = 0;
+        for (int i = 0; i < n; i++) {
+            waterTrapped += min(prefix[i], suffix[i]) - arr[i];
         }
-        return accumulate(ans.begin(),ans.end(),0);
-        
+        return waterTrapped;
     }
 };
