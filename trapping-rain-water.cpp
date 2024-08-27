e2e22b02baa0cd8da3e6847a15d03b290af3f288
yatharthg07 <99501437+yatharthg07@users.noreply.github.com>
2024-08-28 01:55:08 +0530

    Add trapping-rain-water.cpp

---
+++ trapping-rain-water.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n=height.size();
+        vector<int> ans(n,0);
+        vector<int> left(n,-1);
+        vector<int> right(n,-1);
+        int lg=-1;
+        int rg=-1;
+        for(int i=n-1;i>=0;i--)
+        {   
+            if(height[i]>rg)
+            {
+                rg=height[i];
+            }
+            else if(rg>height[i])
+            {
+                right[i]=rg;
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            cout<<right[i]<<" ";
+        }
+        cout<<endl;
+        for(int i=0;i<n;i++)
+        {   
+            if(height[i]>lg)
+            {
+                lg=height[i];
+            }
+            else if(lg>height[i])
+            {
+                left[i]=lg;
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            cout<<left[i]<<" ";
+        }
+        cout<<endl;
+        for(int i=0;i<n;i++)
+        {
+            if(left[i]==-1||right[i]==-1)
+            {
+                continue;
+            }
+            ans[i]=min(left[i],right[i])-height[i];
+        }
+        for(int i=0;i<n;i++)
+        {
+            cout<<ans[i]<<" ";
+        }
+        cout<<endl;
+        cout<< accumulate(ans.begin(),ans.end(),0);
+        return accumulate(ans.begin(),ans.end(),0);
+        
+    }
+};
