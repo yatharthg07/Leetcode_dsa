045e742a478d3636ab61fd7e342bdb333cab6db7
yatharthg07 <99501437+yatharthg07@users.noreply.github.com>
2024-09-08 02:27:13 +0530

    Time: 51 ms (76.56%) | Memory: 38.4 MB (61.01%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -1,42 +1,33 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int p) {
-        int n = tasks.size();
-        unordered_map<char, int> mp;
-        
-        for(char &ch : tasks) {
-            mp[ch]++;
+        int n=tasks.size();
+        vector<int> freq(26, 0);
+        for (char task : tasks) {
+            freq[task - 'A']++;
         }
 
         priority_queue<int> pq; 
-        int time = 0;
         
-        for(auto &it : mp) {
-            pq.push(it.second);
+        for(auto &it : freq) {
+            if(it>0)
+                pq.push(it);
         }
-        
-        while(!pq.empty()) {
-            vector<int> temp;
-            for(int i = 1; i<=p+1; i++) {
-
-                if(!pq.empty()) {
-                    temp.push_back(pq.top()-1); 
-                    pq.pop();
-                }
+        int idlespots= (pq.top()-1)*p;
+        int gapp=pq.top()-1;
+        pq.pop();
+        while(!pq.empty())
+        {
+            int freqq=pq.top();
+            pq.pop();
+            idlespots-=min(freqq,gapp);
+            if(idlespots<=0)
+            {
+                return n;
             }
-            
-            for(int &freq : temp) {
-                if(freq > 0)
-                    pq.push(freq);
-            }
-            
-            if(pq.empty()) 
-                time += temp.size();
-            else
-                time += (p+1); 
-            
         }
+        return idlespots+n;
         
-        return time;
+        
     }
 };
