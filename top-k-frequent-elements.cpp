fd76b4f3f25ca6f17a9c2b2749b61f24567c8df3
yatharthg07 <99501437+yatharthg07@users.noreply.github.com>
2024-09-08 01:51:28 +0530

    Add top-k-frequent-elements.cpp

---
+++ top-k-frequent-elements.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        int n=nums.size();
+        unordered_map<int,int> mp;
+        for(int i=0;i<n;i++)
+        {
+            mp[nums[i]]++;
+        }
+        priority_queue <pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        for(auto it:mp)
+        {
+            pq.push({it.second,it.first});
+            if(pq.size()>k)
+            {
+                pq.pop();
+            }
+        }
+        vector<int> ans;
+        while(!pq.empty())
+        {
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        return ans;
+        
+    }
+};
