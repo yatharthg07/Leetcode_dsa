5ea913ef30acade703a374bcbad82adf5664221a
yatharthg07 <99501437+yatharthg07@users.noreply.github.com>
2024-09-07 20:20:24 +0530

    Add last-stone-weight.cpp

---
+++ last-stone-weight.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq(stones.begin(),stones.end());
+        while(pq.size()>1)
+        {
+            int x=pq.top();
+            pq.pop();
+            int y=pq.top();
+            pq.pop();
+            if(x==y)
+            {
+                continue;
+            }
+            else
+            {
+                pq.push(max(x,y)-min(x,y));
+            }
+        }
+        if(pq.empty())
+        {
+            return 0;
+        }
+        return pq.top();
+        
+    }
+};
