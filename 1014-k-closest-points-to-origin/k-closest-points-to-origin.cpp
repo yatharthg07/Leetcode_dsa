765997a5cae5ca11f86a462644a12f096ac80620
yatharthg07 <99501437+yatharthg07@users.noreply.github.com>
2024-09-07 20:37:30 +0530

    Time: 190 ms (31.96%) | Memory: 75.7 MB (44.65%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        priority_queue<tuple<double,int,int>> pq;
-        for(auto it:points)
-        {
-            pq.push({sqrt((pow(it[0],2)+pow(it[1],2))),it[0],it[1]});
-            if(pq.size()>k)
-            {
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
+        priority_queue<vector<int>, vector<vector<int>>, compare> pq;
+        for (vector<int>& point : points) {
+            pq.push(point);
+            if (pq.size() > K) {
                 pq.pop();
             }
         }
         vector<vector<int>> ans;
-        while(!pq.empty())
-        {
-          auto [d,x,y]=pq.top();
+        while (!pq.empty()) {
+            ans.push_back(pq.top());
             pq.pop();
-            ans.push_back({x,y});
         }
         return ans;
-        
     }
+private:
+    struct compare {
+        bool operator()(vector<int>& p, vector<int>& q) {
+            return p[0] * p[0] + p[1] * p[1] < q[0] * q[0] + q[1] * q[1];
+        }
+    };
 };
