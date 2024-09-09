bb529c5a542e7456e7d40db0194866d864cd5e3f
yatharthg07 <99501437+yatharthg07@users.noreply.github.com>
2024-09-09 14:00:11 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (47.32%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -9,13 +9,15 @@ public:
         }
 
         vector<int>dp(n);
-        dp[0]=nums[0];
-        dp[1]=max(nums[0],nums[1]);
+        int first=nums[0];
+        int second=max(nums[0],nums[1]);
         for(int i=2;i<n;i++)
         {
-            dp[i]=max(dp[i-1],nums[i]+dp[i-2]);
+            int curr=max(second,first+nums[i]);
+            first=second;
+            second=curr;
         }
-        return dp[n-1];
+        return second;
 
 
         
