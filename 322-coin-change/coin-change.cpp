647f5835bfd842df3ecfcf1e9190b6fed602033d
yatharthg07 <99501437+yatharthg07@users.noreply.github.com>
2024-09-11 19:29:20 +0530

    Time: 29 ms (66.89%) | Memory: 16.4 MB (76.05%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -2,27 +2,29 @@ class Solution {
 public:
     int coinChange(vector<int>& arr, int amount) {
         int n=arr.size();
-        vector<vector<int>> dp(n,vector<int> (amount+1,INT_MAX-1));
-        for(int i=0;i<n;i++)
-        {
-            dp[i][0]=0;
-        }
+        vector<int> curr(amount+1,INT_MAX-1);
+        // vector<vector<int>> dp(n,vector<int> (amount+1,INT_MAX-1));
+        // for(int i=0;i<n;i++)
+        // {
+        //     dp[i][0]=0;
+        // }
+        curr[0]=0;
         for(int i=0;i<n;i++)
         {
             for(int j=1;j<=amount;j++)
             {
                 if(i>=1)
                 {
-                    dp[i][j]=dp[i-1][j];
+                    curr[j]=curr[j];
                 }
                 if(j-arr[i]>=0)
                 {
-                    dp[i][j]=min(dp[i][j],dp[i][j-arr[i]]+1);
+                    curr[j]=min(curr[j],curr[j-arr[i]]+1);
                 }
                 
             }
         }
-        return dp[n-1][amount]==INT_MAX-1?-1:dp[n-1][amount];
+        return curr[amount]==INT_MAX-1?-1:curr[amount];
 
         
     }
