dc5abbe88263e553969bcf3847c668c347d4e9e5
yatharthg07 <99501437+yatharthg07@users.noreply.github.com>
2024-09-12 15:00:20 +0530

    Time: 29 ms (59.18%) | Memory: 29.9 MB (56.83%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,19 +1,30 @@
 class Solution {
 public:
+    int rec(vector<vector<int>> &dp,string &s,int l ,int r)
+    {
+        if(l>=r)
+        {
+            return 0;
+        }
+        if(dp[l][r]!=-1)
+        {
+            return dp[l][r];
+        }
+        if(s[l]==s[r])
+        {
+            return dp[l][r]=rec(dp,s,l+1,r-1);
+        }
+        else
+        {
+            return dp[l][r]=1+min(rec(dp,s,l+1,r),rec(dp,s,l,r-1));
+        }
+
+
+    }
+
     int minInsertions(string s) {
         int n = s.size();
-        vector<vector<int>> dp(n, vector<int>(n, 0));
-
-        for (int length = 2; length <= n; length++) {
-            for (int l = 0; l <= n - length; l++) {
-                int r = l + length - 1;
-                if (s[l] == s[r]) {
-                    dp[l][r] = dp[l + 1][r - 1];
-                } else {
-                    dp[l][r] = 1 + min(dp[l + 1][r], dp[l][r - 1]);
-                }
-            }
-        }
-        return dp[0][n - 1];
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+        return rec(dp,s,0,n-1);
     }
 };
