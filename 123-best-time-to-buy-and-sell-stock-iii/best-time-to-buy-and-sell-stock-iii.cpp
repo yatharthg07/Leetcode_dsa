9b461fc6ef063b9e4096ad8a9c0075a06a132dd7
yatharthg07 <99501437+yatharthg07@users.noreply.github.com>
2024-09-12 22:52:43 +0530

    Time: 192 ms (67.16%) | Memory: 126.2 MB (65.91%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -6,14 +6,11 @@ public:
         vector<vector<int>> dp(n+1, vector<int>(4, 0));
 
         for(int i=n-1;i>=0;i--)
-        {
-            for(int j=0;j<4;j++)
-            {   
+        { 
                 dp[i][0]=max(-prices[i]+dp[i+1][1],dp[i+1][0]);
                 dp[i][1]=max(prices[i]+dp[i+1][2],dp[i+1][1]);
                 dp[i][2]=max(-prices[i]+dp[i+1][3],dp[i+1][2]);
-                dp[i][3]=max(prices[i],dp[i+1][3]);
-            }    
+                dp[i][3]=max(prices[i],dp[i+1][3]); 
 
         }
         
