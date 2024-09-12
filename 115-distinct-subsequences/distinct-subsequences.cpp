9d60c9e176f8a0f7a2bde5dd4f660ff58e1e70b8
yatharthg07 <99501437+yatharthg07@users.noreply.github.com>
2024-09-12 18:27:54 +0530

    Time: 7 ms (95.56%) | Memory: 8.1 MB (87.78%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,31 +1,27 @@
-
 #define ll unsigned long long int
 class Solution {
 public:
     int numDistinct(string s, string t) {
-        int n=s.length();
-        int m=t.length();
-        vector<vector<ll>> dp(n+1,vector<ll> (m+1,0));
-        for(ll i=0;i<=n;i++)
-        {
-            dp[i][0]=1;
-        }
+        int n = s.length();
+        int m = t.length();
+        
+        vector<ll> prev(m+1, 0);
+        vector<ll> curr(m+1, 0);
 
-        for(ll i=1;i<=n;i++)
-        {
-            for(ll j=1;j<=m;j++)
-            {
-                if(s[i-1]==t[j-1]){
-                    dp[i][j]=dp[i-1][j]+dp[i-1][j-1];
-                }
-                else
-                {
-                    dp[i][j]=dp[i-1][j];
+        prev[0] = 1;
+        curr[0]= 1;
+
+        for (ll i = 1; i <= n; i++) {
+            for (ll j = 1; j <= m; j++) {
+                if (s[i - 1] == t[j - 1]) {
+                    curr[j] = prev[j] + prev[j - 1]; 
+                } else {
+                    curr[j] = prev[j]; 
                 }
             }
-
+            prev = curr;
         }
-        return dp[n][m];
 
+        return prev[m]; // The result is stored in prev[m]
     }
 };
