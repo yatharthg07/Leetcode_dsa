a82a76c4a94eba28da478be52c97f394f02532c0
yatharthg07 <99501437+yatharthg07@users.noreply.github.com>
2024-09-13 15:07:07 +0530

    Time: 9 ms (64.40%) | Memory: 16.2 MB (64.13%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -4,17 +4,17 @@ public:
         
         int n = prices.size();
         
-        vector<vector<int>> dp(n+1, vector<int>(2*k, 0));
+        vector<vector<int>> dp(n+1, vector<int>(k*2+1, 0));
 
         for(int i=n-1;i>=0;i--)
-        { 
-                for(int j=0;j<2*k-2;j+=2)
-                {    
+        {
+            for(int j=0;j<k*2;j++)
+            {
+                if(j%2==0)
                     dp[i][j]=max(-prices[i]+dp[i+1][j+1],dp[i+1][j]);
-                    dp[i][j+1]=max(prices[i]+dp[i+1][j+2],dp[i+1][j+1]);
-                }
-                dp[i][2*k-2]=max(-prices[i]+dp[i+1][2*k-1],dp[i+1][2*k-2]);
-                dp[i][2*k-1]=max(prices[i],dp[i+1][2*k-1]); 
+                else
+                    dp[i][j]=max(prices[i]+dp[i+1][j+1],dp[i+1][j]);
+            }
 
         }
         
