5271152c20b9f0944a70954d1c1c26ab707b52bf
yatharthg07 <99501437+yatharthg07@users.noreply.github.com>
2024-09-13 22:46:09 +0530

    Time: 35 ms (31.84%) | Memory: 33.1 MB (19.35%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    int countSubstrings(string s) {
-        int n = s.length(), ans = 0;
-        for (int i = 0; i < n; ++i) {
-            int even = palindromeCount(s, i, i + 1);
-            int odd = palindromeCount(s, i, i);
-            ans += even + odd;
-        }
-        return ans;
-    }
 
-    int palindromeCount(const string& s, int left, int right) {
+    int solve(vector<vector<int>>& mem, string& s, int i, int j) {
+        if (i >= j) return 1;
+        if (mem[i][j] >= 0) return mem[i][j];
+        return mem[i][j] = s[i] == s[j] ? solve(mem, s, i+1, j-1) : 0;
+    }
+    
+    int countSubstrings(string s) {
+                vector<vector<int>> mem(s.size(), vector<int>(s.size(), -1));
         int count = 0;
-        while (left >= 0 && right < s.length() && s[left] == s[right]) {
-            --left;
-            ++right;
-            ++count;
+        for(int i = 0; i < s.size(); ++i) {
+            for(int j = i; j < s.size(); ++j) {
+                count += solve(mem, s, i, j);
+            }
         }
         return count;
+        
     }
 };
