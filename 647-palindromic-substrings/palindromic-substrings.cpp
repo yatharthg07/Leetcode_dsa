1871461aa46a1e9122ee8b3c62cbadb14af901b2
yatharthg07 <99501437+yatharthg07@users.noreply.github.com>
2024-09-13 22:38:58 +0530

    Time: 7 ms (73.45%) | Memory: 7.7 MB (97.60%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,25 +1,22 @@
-class Solution 
-{
+class Solution {
 public:
-    int countSubstrings(string s) 
-    {
-        int n = s.length(), count = 0;
-        vector<vector<bool>> dp(n, vector<bool>(n, false));
-        
-        // dp[i][j] represents whether the substring from index i to j is a palindrome
-        for (int i = n - 1; i >= 0; i--) // start from the end of the string
-        {
-            for (int j = i; j < n; j++) // check substrings from i to j
-            {
-                // either length of palindrome is < 3 or inner substring is a palindrome
-                if (s[i] == s[j] && (j - i < 3 || dp[i + 1][j - 1])) 
-                {
-                    dp[i][j] = true; // mark as palindrome
-                    count++;         // increase count
-                }
-            }
+    int countSubstrings(string s) {
+        int n = s.length(), ans = 0;
+        for (int i = 0; i < n; ++i) {
+            int even = palindromeCount(s, i, i + 1);
+            int odd = palindromeCount(s, i, i);
+            ans += even + odd;
         }
-        
+        return ans;
+    }
+
+    int palindromeCount(const string& s, int left, int right) {
+        int count = 0;
+        while (left >= 0 && right < s.length() && s[left] == s[right]) {
+            --left;
+            ++right;
+            ++count;
+        }
         return count;
     }
 };
