143e5546f7ee6cd3dcd1a8fa35e12706f8e3ba42
yatharthg07 <99501437+yatharthg07@users.noreply.github.com>
2024-09-16 00:03:37 +0530

    Time: 20 ms (88.09%) | Memory: 12.6 MB (51.42%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
@@ -1,32 +1,40 @@
 class Solution {
 public:
-    int solve(vector<vector<int>> &dp,vector<int> &cuts,int i,int j)
-    {
-        int ans=1e9;
-        if(i>j)
-        {
-            return 0;
-        }
-        if(dp[i][j]!=-1)
-        {
-            return dp[i][j];
-        }
-        for(int k=i;k<=j;k++)
-        {
+    // int solve(vector<vector<int>>& dp, vector<int>& cuts, int i, int j) {
+    //     int ans = 1e9;
+    //     if (i > j) {
+    //         return 0;
+    //     }
+    //     if (dp[i][j] != -1) {
+    //         return dp[i][j];
+    //     }
+    //     for (int k = i; k <= j; k++) {
 
-                int res=solve(dp,cuts,i,k-1)+solve(dp,cuts,k+1,j)+cuts[j+1]-cuts[i-1];
-                ans=min(res,ans);
-
-        }
-         return dp[i][j]=ans;
-    }
+    //         int res = solve(dp, cuts, i, k - 1) + solve(dp, cuts, k + 1, j) +
+    //                   cuts[j + 1] - cuts[i - 1];
+    //         ans = min(res, ans);
+    //     }
+    //     return dp[i][j] = ans;
+    // }
     int minCost(int n, vector<int>& cuts) {
         cuts.push_back(0);
-        cuts.push_back(n);        
-        sort(cuts.begin(),cuts.end());
-        vector<vector<int>> dp(cuts.size(),vector<int> (cuts.size(),-1));
-        
-        return solve(dp,cuts,1,cuts.size()-2);
-        
+        cuts.push_back(n);
+        sort(cuts.begin(), cuts.end());
+        vector<vector<int>> dp(cuts.size(), vector<int>(cuts.size(), 0));
+        for (int i = cuts.size() - 2; i>=1; i -- ) {
+            for (int j = i; j < cuts.size() - 1; j++) {
+                int ans = 1e9;
+
+                for (int k = i; k <= j; k++) {
+
+                    int res =
+                        dp[i][k - 1] + dp[k + 1][j] + cuts[j + 1] - cuts[i - 1];
+                    ans = min(res, ans);
+                }
+                dp[i][j] = ans;
+            }
+        }
+
+        return dp[1][cuts.size()-2];
     }
 };
