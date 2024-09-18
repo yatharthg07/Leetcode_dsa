3e19f1bd6ef6e39c2927a1ae11d499302df50be4
yatharthg07 <99501437+yatharthg07@users.noreply.github.com>
2024-09-19 03:08:38 +0530

    Add search-insert-position.cpp

---
+++ search-insert-position.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n=nums.size();
+        int low=0;
+        int high=n-1;
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            if(nums[mid]>target)
+            {
+                high=mid-1;
+            }
+            else if(nums[mid]<target)
+            {
+                low=mid+1;
+            }
+            else
+                return mid;
+
+        }
+        return low;
+
+        
+    }
+};
