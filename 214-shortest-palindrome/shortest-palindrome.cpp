7fc1fc59d15afe170d6c1a57455cd8cea77d379b
yatharthg07 <99501437+yatharthg07@users.noreply.github.com>
2024-09-20 23:13:02 +0530

    Time: 176 ms (5.09%) | Memory: 9.5 MB (92.97%) - LeetSync

--- 214-shortest-palindrome/shortest-palindrome.cpp
+++ 214-shortest-palindrome/shortest-palindrome.cpp
@@ -14,26 +14,21 @@ public:
     string shortestPalindrome(string s) {
         int n = s.length();
         if (n == 0) return s;
-        
-        int maxPalindromeLength = 0;
-        
-        // Check for the longest palindrome starting from index 0
-        for (int i = n - 1; i >= 0; i--) {
-            if (s[0] == s[i] && ispal(s, 0, i)) {
-                maxPalindromeLength = i + 1;
+        int idx;
+        for(int i=n-1;i>=0;i--)
+        {
+            idx=i;
+            if(s[i]==s[0] && ispal(s,0,i))
+            {
                 break;
             }
         }
-        
-        // If the entire string is a palindrome, return it as is
-        if (maxPalindromeLength == n) {
+        if(idx==n-1)
+        {
             return s;
         }
-        
-        // Reverse the remaining suffix and add it to the front
-        string suffix = s.substr(maxPalindromeLength);
-        reverse(suffix.begin(), suffix.end());
-        
-        return suffix + s;
+        string temp=s.substr(idx+1);
+        reverse(temp.begin(),temp.end());
+        return temp+s;
     }
 };
