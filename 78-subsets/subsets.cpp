773bb8c9c8fe374fd6416af9e95994ee899835b9
yatharthg07 <99501437+yatharthg07@users.noreply.github.com>
2024-09-28 19:35:38 +0530

    Time: 4 ms (39.25%) | Memory: 8.7 MB (53.17%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    void rec(vector<vector<int>> &ans,vector<int> &temp,vector<int> &nums,int n)
-    {
-        if(n<0)
-        {
-            ans.push_back(temp);
-            return;
-        }
-        temp.push_back(nums[n]);
-        rec(ans,temp,nums,n-1);
-        temp.pop_back();
-        rec(ans,temp,nums,n-1);
-    }
     vector<vector<int>> subsets(vector<int>& nums) {
-        int n=nums.size();
         vector<vector<int>> ans;
-        vector<int> temp;
-        rec(ans,temp,nums,n-1);
+        int n=nums.size();
+        int sets=pow(2,n);
+        for(int i=0;i<sets;i++)
+        {
+            vector<int> temp;
+            for(int j=0;j<n;j++)
+            {
+                if(i&(1<<j))
+                {
+                    temp.push_back(nums[j]);
+                }
+            }
+            ans.push_back(temp);
+        }
         return ans;
+        
     }
 };
