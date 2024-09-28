fbc8429e15f9938ce77b4db4a6327c6560542253
yatharthg07 <99501437+yatharthg07@users.noreply.github.com>
2024-09-28 19:59:10 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (59.05%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -1,23 +1,14 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int ans=0;
-        for(int i=0;i<32;i++)
+        sort(nums.begin(),nums.end());
+        for(int i=1;i<nums.size();i+=3)
         {
-            int cnt=0;
-            for(auto num:nums)
+            if(nums[i]!=nums[i-1])
             {
-                if(num&(1<<i))
-                {
-                    cnt++;
-                }
-            }
-            if(cnt%3==1)
-            {
-                ans=ans|(1<<i);
+                return nums[i-1];
             }
         }
-        return ans;
-        
+        return nums[nums.size()-1];
     }
 };
