58fd7066cf5449510f57b20774f6d78b1455d374
yatharthg07 <99501437+yatharthg07@users.noreply.github.com>
2024-10-01 04:11:57 +0530

    Time: 10 ms (59.85%), Space: 14312000 (9.35%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,59 +1,24 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        int n = s.length();
+        unordered_map<char, int> m;
+        
+        m['I'] = 1;
+        m['V'] = 5;
+        m['X'] = 10;
+        m['L'] = 50;
+        m['C'] = 100;
+        m['D'] = 500;
+        m['M'] = 1000;
+        
         int ans = 0;
-        int i = 0;
-        while (i < n) {
-            if (s[i] == 'I') {
-                if (i + 1 < n && s[i + 1] == 'V') {
-                    ans += 4;
-                    i += 2;
-                } else if (i + 1 < n && s[i + 1] == 'X') {
-                    ans += 9;
-                    i += 2;
-                } else {
-                    ans += 1;
-                    i++;
-                }
-            } else if (s[i] == 'V') {
-                ans += 5;
-                i++;
+        
+        for(int i = 0; i < s.length(); i++){
+            if(m[s[i]] < m[s[i+1]]){
+                ans -= m[s[i]];
             }
-
-            else if (s[i] == 'X') {
-                if (i + 1 < n && s[i + 1] == 'L') {
-                    ans += 40;
-                    i += 2;
-                } else if (i + 1 < n && s[i + 1] == 'C') {
-                    ans += 90;
-                    i += 2;
-                } else {
-                    ans += 10;
-                    i++;
-                }
-            } else if (s[i] == 'L') {
-                ans += 50;
-                i++;
-            }
-
-            else if (s[i] == 'C') {
-                if (i + 1 < n && s[i + 1] == 'D') {
-                    ans += 400;
-                    i += 2;
-                } else if (i + 1 < n && s[i + 1] == 'M') {
-                    ans += 900;
-                    i += 2;
-                } else {
-                    ans += 100;
-                    i++;
-                }
-            } else if (s[i] == 'D') {
-                ans += 500;
-                i++;
-            } else if (s[i] == 'M') {
-                ans += 1000;
-                i++;
+            else{
+                ans += m[s[i]];
             }
         }
         return ans;
