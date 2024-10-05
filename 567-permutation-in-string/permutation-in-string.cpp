a7402743b5be06411acadbf82bdfb356f69890e1
yatharthg07 <99501437+yatharthg07@users.noreply.github.com>
2024-10-06 02:23:45 +0530

    Time: 14 ms (31.72%) | Memory: 10 MB (35.86%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,52 +1,42 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        int n=s2.length();
-        unordered_map<char,int> mp;
-        for(auto c:s1)
-        {
+        int n = s2.length();
+        unordered_map<char, int> mp;
+        
+        for (auto c : s1) {
             mp[c]++;
         }
-        int l=0;
-        int r=0;
-        while(r!=n && l!=n)
-        {
-            if(mp.find(s2[r])!=mp.end() )
-            {
+
+        int l = 0, r = 0;
+
+        while (r < n && l < n) {
+            if (mp.find(s2[r]) != mp.end()) {
                 mp[s2[r]]--;
             }
-            if(mp.find(s2[r])==mp.end())
-            {
-                if(l!=n)
-                {
-                    for(int i=l;i<=r;i++)
-                    {
-                        if(mp.find(s2[i])!=mp.end())
-                        {
-                            mp[s2[i]]++;
-                        }
+            if (mp.find(s2[r]) == mp.end()) {
+                for (int i = l; i <= r; i++) {
+                    if (mp.find(s2[i]) != mp.end()) {
+                        mp[s2[i]]++;
                     }
                 }
-                l=r+1;
+                l = r + 1;
             }
-            else if(mp[s2[r]]<0)
-            {
-                while(l!=n && mp[s2[r]]<0)
-                {
-                        if(mp.find(s2[l])!=mp.end())
-                        {
-                            mp[s2[l]]++;
-                        }
-                        l++;
+            else if (mp[s2[r]] < 0) {
+                while (l < n && mp[s2[r]] < 0) {
+                    if (mp.find(s2[l]) != mp.end()) {
+                        mp[s2[l]]++;
+                    }
+                    l++;
                 }
             }
-            if(r-l+1==s1.length())
-            {
+            if (r - l + 1 == s1.length()) {
                 return true;
             }
-            r++;
 
+            r++;
         }
+
         return false;
     }
 };
