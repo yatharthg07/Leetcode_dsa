9c45c6cc34768faf9069777dcb0441168cfb9710
yatharthg07 <99501437+yatharthg07@users.noreply.github.com>
2024-10-12 23:43:16 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (89.40%) - LeetSync

--- 133-clone-graph/clone-graph.cpp
+++ 133-clone-graph/clone-graph.cpp
@@ -1,50 +1,21 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    vector<Node*> neighbors;
-    Node() {
-        val = 0;
-        neighbors = vector<Node*>();
-    }
-    Node(int _val) {
-        val = _val;
-        neighbors = vector<Node*>();
-    }
-    Node(int _val, vector<Node*> _neighbors) {
-        val = _val;
-        neighbors = _neighbors;
-    }
-};
-*/
-
 class Solution {
 public:
-    Node* cloneGraph(Node* node) {
-                if (node == NULL)
-            return NULL;
-
-        unordered_map<Node*,Node*> clone;
-        queue<Node*> q;
-        q.push(node);
-        clone[node]=new Node(node->val);
-        while(!q.empty())
-        {
-            auto curr=q.front();
-            q.pop();
-            for(auto it:curr->neighbors)
-            {
-                if(clone.find(it)==clone.end())
-                {
-                    auto newnode=new Node(it->val);
-                    clone[it]=newnode;
-                    q.push(it);
-                }
-                clone[curr]->neighbors.push_back(clone[it]);
-            }        
+    unordered_map<Node*,Node*> mp;
+    void  dfs(Node* node){
+        Node* copy=new Node(node->val);
+        mp[node]=copy;
+        for(auto ng: node->neighbors){
+            if(mp.find(ng)!=mp.end()){
+                copy->neighbors.push_back(mp[ng]);
+            }else{
+                dfs(ng);
+                copy->neighbors.push_back(mp[ng]);
+            }
         }
-        return clone[node];
-        
+    }
+    Node* cloneGraph(Node* node) {
+        if(node==NULL) return NULL;
+        dfs(node);
+       return mp[node];
     }
 };
