291954e7e90ab2f6c2f216fdb665a884fb329e52
yatharthg07 <99501437+yatharthg07@users.noreply.github.com>
2024-10-12 21:58:01 +0530

    Time: 123 ms (96.25%) | Memory: 54.9 MB (96.59%) - LeetSync

--- 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.cpp
+++ 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.cpp
@@ -1,37 +1,48 @@
-
 class Solution {
 public:
-int smallestChair(vector<vector<int>>& times, int t) {
-        int arrival = times[t][0];
-        sort(times.begin(), times.end());
-        set<int> avilableSeats;
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        int newSeat = 0;
-        int result;
-        for(int i=0; i<times.size(); i++) {
-            int seat=-1;
-            while(!pq.empty() && pq.top().first <= times[i][0]) {
-                seat = pq.top().second;
-                pq.pop();
-                avilableSeats.insert(seat);
+    typedef pair<int, int> P;
+
+    int smallestChair(vector<vector<int>>& times, int targetFriend) {
+        
+        int n = times.size();
+        priority_queue<P, vector<P>, greater<P> > occupied; //{departTime, chairNo}
+        priority_queue<int, vector<int>, greater<int>> free; //min heap of unoccupied chairs
+        
+        int targetFriendArrival = times[targetFriend][0];
+		
+        //Sort based on arrival time
+        sort(times.begin(),times.end());
+        
+		int chairNo = 0;
+
+        for(int i = 0; i < n; i++) {
+            int arrival  = times[i][0];
+            int depart   = times[i][1];
+            
+            //free chairs accordingly
+            while(!occupied.empty() && occupied.top().first <= arrival) {
+                free.push(occupied.top().second); //this chair is now free
+                occupied.pop();
             }
-            if(avilableSeats.size() > 0) {
-                seat = *(avilableSeats.begin());
-                avilableSeats.erase(seat);
-            }
-            else {
-                seat = newSeat++;
-            }
-            pq.push({times[i][1], seat});
-            if(times[i][0] == arrival) {
-                result = seat;
-                break;
+
+            if(free.empty()) {
+                occupied.push({depart, chairNo});
+
+                if(arrival == targetFriendArrival)
+                    return chairNo;
+
+                chairNo++;
+            } else {
+                int leastChairAvailable = free.top();
+                free.pop();
+                if(arrival == targetFriendArrival) {
+                    return leastChairAvailable;
+                }
+                occupied.push({depart, leastChairAvailable});
             }
         }
-        return result;
-    }
-    
 
-        
+        return -1;
+    }
     
 };
