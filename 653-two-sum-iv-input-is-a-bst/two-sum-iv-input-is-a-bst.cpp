fcf66beb707dc7df3224586866ca52b8e5b67036
yatharthg07 <99501437+yatharthg07@users.noreply.github.com>
2024-10-20 02:25:25 +0530

    Time: 0 ms (100.00%) | Memory: 37.3 MB (30.23%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
@@ -1,26 +1,36 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+class BSTIterator {
+private:
+    stack<TreeNode*> st;
+    bool reverse = false;
+    void push(TreeNode* root) {
+        while (root != nullptr) {
+            st.push(root);
+            root = !reverse ? root->left : root->right;
+        }
+    }
+public:
+    BSTIterator(TreeNode* root, bool reverse = false) : reverse(reverse) {
+        push(root);
+    }
+    int next() {
+        TreeNode* top = st.top(); st.pop();
+        push(!reverse ? top->right : top->left);
+        return top->val;
+    }
+};
+
 class Solution {
 public:
-    unordered_set<int> mp;
-    bool solve(TreeNode*root,int k)
-    {
-        if(root==NULL) return 0;
-        if(mp.find(k-root->val)!=mp.end())return 1;
-        mp.insert(root->val);
-        return solve(root->left,k)||solve(root->right,k);    
-    }
     bool findTarget(TreeNode* root, int k) {
-        return solve(root,k);
-        
+        BSTIterator leftItr(root), rightItr(root, true);
+        int left = leftItr.next(), right = rightItr.next();
+        while (left < right) {
+            if (left + right == k) return true;
+            if (left + right < k)
+                left = leftItr.next();
+            else
+                right = rightItr.next();
+        }
+        return false;
     }
 };
